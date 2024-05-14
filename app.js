68780e0ca26f615146b14f1b672dd56b77c49440
Asad Siddiqui <49947509+asadsid56@users.noreply.github.com>
2024-05-14 10:27:11 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 let inputBox = document.getElementById("inputBox"); 
 let add = document.querySelector("#add"); 
-let listContainer = document.querySelector("#listContainer"); 
 let listItems = document.querySelector("#listItems"); 
-
+let listContainer = document.querySelector("#listContainer")
 add.addEventListener("click", () => {
     addTask();
 }); 
@@ -16,14 +15,13 @@ function addTask() {
         alert("Please write something");
     } else {
 
-        let listItems = document.querySelector("#listItems"); 
         listItems.insertAdjacentHTML("beforeend", `
         <div class="row">
             <div class="col-lg-12">
                 <div id="listContainer" class="mb-3">
                   <li class="card">${inputValue}</li>
-                  <button type="button" class="btn btn-warning btn-lg"><i class="fa-solid fa-pen-to-square"></i></button>
-                  <button type="button" class="btn btn-danger btn-lg"><i class="fa-solid fa-trash"></i></button>
+                  <button onClick="edit()" type="button" class="btn btn-warning btn-lg"><i class="fa-solid fa-pen-to-square"></i></button>
+                  <button onClick="deletePost()" type="button" class="btn btn-danger btn-lg"><i class="fa-solid fa-trash"></i></button>
                 </div>
             </div>
           </div>
@@ -32,6 +30,31 @@ function addTask() {
     }
 
     inputBox.value = ""; 
+    save(); 
 
 }   
 
+function deletePost() {
+
+    listItems.addEventListener("click", (e) => {
+        // Check where are you clicking
+        // console.log(e.target, "i was clicked"); 
+        e.target.parentElement.parentElement.remove()
+        save(); 
+    });  
+}
+
+/* function edit() {
+    console.log("i was clicked");
+} */
+
+
+function save() {
+    localStorage.setItem("data", listItems.innerHTML); 
+}
+
+function showData() {
+    listItems.innerHTML = localStorage.getItem("data"); 
+}
+
+showData(); 
